55adc987ef8b23d3fc3323f09983a6b3dcd5a067
pravalika <pravalika@DESKTOP-18DE0TD>
2025-12-25 18:36:50 +0530

    Loops

---
+++ Sriindu_TNS/src/com/tnsif/intro/dowhile.java
@@ -0,0 +1,14 @@
+package com.tnsif.intro;
+
+public class dowhile {
+	public static void main(String[] args)
+	{
+		int a = 0;
+		do
+		{
+			System.out.println("*");
+			a++;
+		}while (a<=21);
+	}
+}
+
