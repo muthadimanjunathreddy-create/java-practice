e7b669200dba8e6498ec6957badc26b2fba5c7c8
pravalika <pravalika@DESKTOP-18DE0TD>
2025-12-29 17:47:45 +0530

    Reverse of String

---
+++ Sriindu_TNS/src/com/tnsif/intro/ReverseofString.java
@@ -0,0 +1,20 @@
+package com.tnsif.intro;
+import java.util.Scanner;
+public class ReverseofString {
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enetr the String: ");
+		String str = sc.nextLine();
+		String rev = "";
+		for(int i = str.length()-1; i>=0; i--)
+		{
+			rev = rev + str.charAt(i);
+		}
+		System.out.println(rev);
+		
+		
+		
+	}
+
+}
