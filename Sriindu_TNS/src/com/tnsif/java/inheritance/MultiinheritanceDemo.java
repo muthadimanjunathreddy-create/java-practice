b2eff01f94412a403e4c9e185a546440b029bbc0
pravalika <pravalika@DESKTOP-18DE0TD>
2026-01-26 11:33:35 +0530

    MultiLevel Inheritence

---
+++ Sriindu_TNS/src/com/tnsif/java/inheritance/MultiinheritanceDemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.java.inheritance;
+
+public class MultiinheritanceDemo {
+		public static void main(String[]args) {
+		Child c = new Child();
+		c.display();
+		c.show();
+		c.print();
+		System.out.println("This is Demo class");
+			
+		}
+
+}
